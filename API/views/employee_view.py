32e9bef99bd790a369be28709b1f52e4a177a5b1
Mahabubul Hasan <shourav6688@gmail.com>
2023-10-25 21:26:37 +0600

    added cast vote APIView

--- API/views/employee_view.py
+++ API/views/employee_view.py
@@ -3,10 +3,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 
+from applibs.helpers import check_time_limit_validity_for_voting
 from applibs.logging_utils import get_logger
 
-from API.models import Employee
-from API.serializers import CreateEmployeeSerializer
+from API.models import Employee, Menu, Vote
+from API.serializers import CreateEmployeeSerializer, CastVoteSerializer
 
 logger = get_logger(__name__)
 
@@ -39,3 +40,55 @@ class CreateEmployeeAPIView(APIView):
         output = self.save_employee()
         return Response(output)
 
+
+class CastVoteAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def __init__(self):
+        super(CastVoteAPIView, self).__init__()
+        self.request = None
+        self.serializer_data = dict()
+        self.serializer_class = CastVoteSerializer
+        self.voter_obj = None
+        self.vote_obj = None
+
+    def check_voting_eligibility(self):
+        self.voter_obj = Employee.objects.get_employee(employee_id=self.serializer_data["voter_id"])
+        if not self.voter_obj:
+            return status.HTTP_406_NOT_ACCEPTABLE
+
+        time_validity = check_time_limit_validity_for_voting()
+
+        if time_validity:
+            is_vote_acceptable = Menu.objects.get_eligible_menu_for_vote()
+            if is_vote_acceptable:
+                self.preprocess_vote()
+
+        return status.HTTP_406_NOT_ACCEPTABLE
+
+    def preprocess_vote(self):
+        self.vote_obj = Employee.objects.prepare_menu_vote(payload=self.serializer_data["vote_for"])
+        if self.vote_obj:
+            self.cast_vote()
+
+        return status.HTTP_424_FAILED_DEPENDENCY
+
+    def cast_vote(self):
+        self.serializer_data["voter_id"] = self.voter_obj
+        self.serializer_data["voter_for"] = self.vote_obj
+        vote_casted = Vote.objects.cast_vote(payload=self.serializer_data)
+        if vote_casted:
+            return status.HTTP_201_CREATED
+        return status.HTTP_424_FAILED_DEPENDENCY
+
+    def post(self, request):
+        self.request = request
+        serializer = self.serializer_class(data=self.request.data)
+
+        if not serializer.is_valid():
+            logger.debug({"serializer_error": repr(serializer.errors)})
+            return Response("Invalid data format", status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        self.serializer_data = dict(serializer.validated_data)
+        output = self.check_voting_eligibility()
+        return Response(output)
