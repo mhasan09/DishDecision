d0dc02b725984a6e743ad3386856fb616585e440
Mahabubul Hasan <shourav6688@gmail.com>
2023-10-25 21:49:38 +0600

    added check voter orm method

--- API/manager/vote_manager.py
+++ API/manager/vote_manager.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 from applibs.logging_utils import get_logger
 
 logger = get_logger(__name__)
@@ -17,4 +17,13 @@ class VoteManager(models.Manager):
             logger.debug({"payload": payload, "db_exception_error": repr(e)})
             return None
 
+    def check_voter(self, payload):
+        try:
+            return self.filter(
+                voter_id=payload["voter_id"],
+                created_at__day=date.today().day,
+            ).exists()
 
+        except Exception as e:
+            logger.debug({"payload": payload, "db_exception_error": repr(e)})
+            return None
