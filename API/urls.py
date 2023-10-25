8b2319d20e111eaf1cf00733ccc594bc4ab9d137
Mahabubul Hasan <shourav6688@gmail.com>
2023-10-25 21:49:57 +0600

    casting vote api refactor and urls added

--- API/urls.py
+++ API/urls.py
@@ -5,7 +5,7 @@ from API.views.token_view import (
     RefreshTokenAPIView,
 )
 from API.views.restaurant_management_view import CreateRestaurantAPIView, UploadMenuAPIView, GetMenuAPIView
-from API.views.employee_view import CreateEmployeeAPIView
+from API.views.employee_view import CreateEmployeeAPIView, CastVoteAPIView
 
 urlpatterns = [
     path("v1/auth/token/", TokenAPIView.as_view(), name="access-token"),
@@ -14,4 +14,5 @@ urlpatterns = [
     path("v1/upload-menu/", UploadMenuAPIView.as_view(), name="upload-menu"),
     path("v1/create-employee/", CreateEmployeeAPIView.as_view(), name="create-employee"),
     path("v1/get-menu/", GetMenuAPIView.as_view(), name="get-menu"),
+    path("v1/cast-vote/", CastVoteAPIView.as_view(), name="cast-vote"),
 ]
--- API/views/employee_view.py
+++ API/views/employee_view.py
@@ -51,34 +51,45 @@ class CastVoteAPIView(APIView):
         self.serializer_class = CastVoteSerializer
         self.voter_obj = None
         self.vote_obj = None
+        self.voter_eligible_payload = dict()
 
     def check_voting_eligibility(self):
         self.voter_obj = Employee.objects.get_employee(employee_id=self.serializer_data["voter_id"])
+
+        self.voter_eligible_payload["voter_id"] = self.voter_obj
+        voter_already_voted = Vote.objects.check_voter(payload=self.voter_eligible_payload)
+
+        if voter_already_voted:
+            return status.HTTP_406_NOT_ACCEPTABLE
+
         if not self.voter_obj:
             return status.HTTP_406_NOT_ACCEPTABLE
 
         time_validity = check_time_limit_validity_for_voting()
-
         if time_validity:
             is_vote_acceptable = Menu.objects.get_eligible_menu_for_vote()
             if is_vote_acceptable:
                 self.preprocess_vote()
+                return status.HTTP_201_CREATED
+            else:
+                return status.HTTP_406_NOT_ACCEPTABLE
 
         return status.HTTP_406_NOT_ACCEPTABLE
 
     def preprocess_vote(self):
-        self.vote_obj = Employee.objects.prepare_menu_vote(payload=self.serializer_data["vote_for"])
+        self.vote_obj = Menu.objects.prepare_menu_vote(menu_id=self.serializer_data["vote_for"])
         if self.vote_obj:
             self.cast_vote()
+            return None
 
         return status.HTTP_424_FAILED_DEPENDENCY
 
     def cast_vote(self):
         self.serializer_data["voter_id"] = self.voter_obj
-        self.serializer_data["voter_for"] = self.vote_obj
+        self.serializer_data["vote_for"] = self.vote_obj
         vote_casted = Vote.objects.cast_vote(payload=self.serializer_data)
         if vote_casted:
-            return status.HTTP_201_CREATED
+            return None
         return status.HTTP_424_FAILED_DEPENDENCY
 
     def post(self, request):
