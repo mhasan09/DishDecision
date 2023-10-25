444c09839a315e3fb37e778d9db3da838259f8bc
Mahabubul Hasan <shourav6688@gmail.com>
2023-10-25 21:26:15 +0600

    added timer check for vote casting time period

--- applibs/helpers.py
+++ applibs/helpers.py
@@ -5,11 +5,23 @@ def string_to_datetime(datetime_string, format="%Y-%m-%d %H:%M:%S.%f"):
     return datetime.strptime(datetime_string, format)
 
 
-def check_time_limit_validity():
+def check_time_limit_validity_for_uploading_menu():
     current_time = datetime.now().time()
     # Define the time ranges for midnight (12:00 AM) and 10:00 AM
     midnight = time(0, 0)
     ten_am = time(10, 0)
+
+    # Check if the current time is between midnight and 10:00 AM
+    if midnight <= current_time < ten_am:
+        return True
+    return False
+
+
+def check_time_limit_validity_for_voting():
+    current_time = datetime.now().time()
+    # Define the time ranges for midnight (12:00 AM) and 10:00 AM
+    midnight = time(10, 0)
+    ten_am = time(12, 0)
 
     # Check if the current time is between midnight and 10:00 AM
     if midnight <= current_time < ten_am:
