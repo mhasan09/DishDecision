a098cc9d0e74adacd31a7234ebeb457274bb8623
Mahabubul Hasan <shourav6688@gmail.com>
2023-10-25 23:31:23 +0600

    added get winner manager method

--- API/manager/vote_manager.py
+++ API/manager/vote_manager.py
@@ -1,5 +1,8 @@
 from django.db import models
 from datetime import date
+
+from django.db.models import Count
+
 from applibs.logging_utils import get_logger
 
 logger = get_logger(__name__)
@@ -26,4 +29,13 @@ class VoteManager(models.Manager):
 
         except Exception as e:
             logger.debug({"payload": payload, "db_exception_error": repr(e)})
+            return None
+
+    def get_winner(self):
+        try:
+            reports = self.filter(created_at__day=date.today().day)
+            return list(reports.values("vote_for").annotate(total_vote_count=Count('id')))
+
+        except Exception as e:
+            logger.debug(repr(e))
             return None
